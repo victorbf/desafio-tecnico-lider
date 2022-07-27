729de349e6fcd660f0c811dbdbb465cc9065f1ba
victorbf <victorbeiraof@gmail.com>
2022-07-27 04:33:13 -0300

    chore: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import Hello from '~/components/Hello';
+import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import GlobalStyle from './styles/globalStyles';
+import Login from './pages/Login';
+import { DefaultLayout } from './layouts';
 
 const App = () => {
-  return <Hello />;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<DefaultLayout />}>
+          <Route
+            path='/'
+            element={<ProtectedRoute authenticationPath='/login' outlet={<Home />} />}
+          />
+          <Route
+            path='cart'
+            element={<ProtectedRoute authenticationPath='/login' outlet={<Cart />} />}
+          />
+        </Route>
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
