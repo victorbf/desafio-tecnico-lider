ec9bf7338cb89c9601f48ec8e78c3b4d20172973
victorbf <victorbeiraof@gmail.com>
2022-07-27 04:33:05 -0300

    chore: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CartItemsProvider } from './contexts/CartItemsContext';
 import App from './App';
+import { ModalProvider } from './contexts/ModalContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <CartItemsProvider>
-      <App />
-    </CartItemsProvider>
+    <div style={{ position: 'relative' }}>
+      <CartItemsProvider>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </CartItemsProvider>
+    </div>
   </BrowserRouter>,
 );
